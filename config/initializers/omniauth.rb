c4d7e55980c27e00cc04e3d94453e61fa313cd78
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-10-04 21:07:01 -0400

    Add omniauth config

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, 
+  ENV['GOOGLE_CLIENT_ID'],
+  ENV['GOOGLE_CLIENT_SECRET'],
+  {
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
+    redirect_uri:'http://localhost:3000/auth/google/callback'
+  }
+end
