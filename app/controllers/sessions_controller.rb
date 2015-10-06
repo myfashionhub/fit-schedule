e571026953932b50077926ae81b992f4962606d3
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-10-05 22:54:32 -0400

    Add users controller
    Add user show template + current_user helper method.
    Find or create user in sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_or_create(request.env["omniauth.auth"])
-    binding.pry
-    client = Google::APIClient.new
-    client.authorization.access_token = user.google_token
-    service = client.discovered_api('calendar', 'v3')
-    
-    result = client.execute(
-      :api_method => service.calendar_list.list,
-      :parameters => {},
-      :headers => {'Content-Type' => 'application/json'}
-    )
-    binding.pry
-    puts "RESULT #{result.data}"
+    session[:user_id] = user.id
+    redirect_to '/schedule'
   end
 
 end
