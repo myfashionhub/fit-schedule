61074ffe6a9f7d4770b1606bc6e28f689edd867b
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-10-12 00:15:17 -0400

    Add create method for class model & controller

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -7,11 +7,20 @@ class ClassesController < ApplicationController
   end
 
   def create
-    puts params
-    studio_name = params[:url].split('.')[1].downcase.capitalize
-    scraper_class = "Scraper::#{studio_name}"
-    result = scraper_class.constantize.get_classes(params[:url])
-    render json: { classes: result }
+    provider = params[:url].split('.')[1].downcase.capitalize
+    scraper_class = "Scraper::#{provider}"
+    classes = nil
+
+    studio = Studio.find_by(schedule_url: params[:url])
+    if studio
+      classes = studio.all_classes
+    else
+      result = scraper_class.constantize.get_classes(params[:url])
+      studio = result[:studio]
+      #classes = Class.create_from_raw(result) if studio.id.present?
+    end
+    
+    render json: { studio: studio, classes: result[:classes] }
   end
 
   
--- app/models/class.rb
+++ app/models/class.rb
@@ -1,6 +1,19 @@
 class Class < ActiveRecord::Base
   belongs_to :studio
   has_many   :appointments
-
   
+  def self.create_from_raw(data)
+    data[:classes].map do |klass|
+      Class.find_or_create_by({
+        name:       klass["name"],
+        date:       klass["date"],
+        start_time: klass["start_time"],
+        end_time:   klass["end_time"],
+        instructor: klass['instructor'],
+        duration:   klass["duration"],
+        studio_id:  data[:studio].id
+      })
+    end
+  end
+
 end
