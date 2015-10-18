bfeb0b2df7ff0941642c85d9cab3c80ee4463f3f
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-10-17 21:11:18 -0400

    Implement sign out button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,9 @@ class SessionsController < ApplicationController
     redirect_to '/schedule'
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'sessions#new'
   get "/auth/:provider/callback" => "sessions#create"
+  delete '/sessions' => 'sessions#destroy', as: 'delete_session'
 
   get '/schedule' => 'users#show'
   resources :calendars, only: [:index, :show, :update]
