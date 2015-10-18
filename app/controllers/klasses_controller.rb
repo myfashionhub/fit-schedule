27b3b5300c9292f010e8258af1ee5d5034ac75fc
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-10-17 23:28:38 -0400

    Make sure to parse classes if studio is new

--- app/controllers/klasses_controller.rb
+++ app/controllers/klasses_controller.rb
@@ -11,15 +11,17 @@ class KlassesController < ApplicationController
     scraper_class = "Scraper::#{provider}"
     classes = nil
     result = nil
+    new_studio = false
 
     studio = Studio.find_by(schedule_url: params[:url])
     
     if !studio
       result = scraper_class.constantize.get_classes(params[:url])
       studio = result[:studio]
+      new_studio = true
     end
     
-    if studio.updated_at.nil? || studio.updated_at < Time.now - 21600
+    if new_studio || studio.updated_at < Time.now - 21600
       result = scraper_class.constantize.get_classes(params[:url]) if result.nil?
       classes = Klass.create_from_raw(result)
     else
