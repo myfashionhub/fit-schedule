b8dc7cc765dcf8049e265f1ec8f401c833f2defe
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-10-22 18:37:47 -0400

    Divide customize page into sections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
 
-  def show
-  end
-
   def update
     current_user.update(calendar_id: params[:calendar_id]) if current_user
+  end
+
+  def schedule
+  end
+
+  def customize
   end
 
 end
