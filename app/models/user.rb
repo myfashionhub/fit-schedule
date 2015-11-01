314d600f62e8a2afa13fcb1723130b725113921b
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-11-01 16:55:22 -0500

    Add validations & AR relationships to User
    Add google_uid when creating user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
 
+  has_many :appointments
+  has_many :klasses, through: :appointments
+
+  validates_uniqueness_of :email
+  validates :google_uid, presence: true
+
   def self.find_or_create(auth_hash)
     user = User.find_by(google_uid: auth_hash['uid'])
     
@@ -8,6 +14,7 @@ class User < ActiveRecord::Base
         name: auth_hash['info']['name'],
         email: auth_hash['info']['email'],
         image: auth_hash['info']['image'],
+        google_uid: auth_hash['uid'],
         google_token: auth_hash['credentials']['token']
       )
     else
