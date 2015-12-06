122d56f433f66845ae13797ba05ee7bf9f2cc8f1
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-12-05 20:40:30 -0500

    Make calendar request when choosing the tab

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -13,12 +13,18 @@ function Navigation(ul, sectionSelector) {
   this.toggle = function() {
     navLis.click(function(e) {
       var index = navLis.index(e.target);
-      
+
       $(sectionSelector+'.active').removeClass('active');
       ul.find('li.active').removeClass('active');
 
       $(e.target).addClass('active');
-      $($(sectionSelector)[index]).addClass('active');      
+      var targetSection = $($(sectionSelector)[index]);
+      targetSection.addClass('active');
+
+      if (targetSection.attr('class').indexOf('calendars') > -1) {
+        var cal = new Calendar();
+        cal.getAllCalendars();
+      }
     });
   };
 
