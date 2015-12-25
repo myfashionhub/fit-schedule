da2979df72aad46c90daa32ee1a9847a88ede19d
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-12-25 17:56:59 -0500

    Add fresh_token & token_expires to user

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   ENV['FS_GOOGLE_SECRET'],
   {    
     access_type: 'offline',
+    approval_prompt: 'force',
     scope: 'https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/calendar',
     redirect_uri: ENV['API_URL']+'/auth/google_oauth2/callback'
   }
