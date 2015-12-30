d713be53e3cbd529098f7a1046b5ba0f8c84710e
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-12-30 16:14:22 -0500

    Refactor navigation to use browser hash

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,26 @@
 //= require underscore
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function() {
+  var customizeTabs = [ 'studio', 'availability', 'calendar' ];
+  var scheduleTabs = [ 'classes', 'studios' ];
+  window.customizeNav = new Navigation(
+    customizeTabs, $('.customize-nav'), '.customize-wrapper section'
+  );
+  window.scheduleNav = new Navigation(
+    scheduleTabs, $('.schedule-nav'), '.schedule-wrapper section'
+  );
+
+  listenForHashChange();
+});
+
+function listenForHashChange() {
+  $(window).on('hashchange', function() {
+    if ( window.location.pathname === '/customize' ) {
+      window.customizeNav.toggleByHash();
+    } else if ( window.location.pathname === '/schedule' ) {
+      window.scheduleNav.toggleByHash();
+    }
+  });
+}
