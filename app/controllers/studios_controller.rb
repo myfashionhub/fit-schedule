b29ef92b7ac49b7410abe31b0dc6a26b3a278d13
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2016-08-14 19:30:12 -0400

    Modify studio controller & rake task

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -4,19 +4,22 @@ class StudiosController < ApplicationController
     studio_id = params[:id]
     studio = Studio.find(studio_id)
 
-    classes = Rails.cache.fetch("#{studio_id}/classes", expires_in: 2.hours) do
+    classes = Rails.cache.fetch(
+      "studios/#{studio_id}/classes", expires_in: 2.hours
+    ) do
       Klass.where(studio_id: studio_id).where('date >= ?', Date.today)
     end
 
     unique_classes = Rails.cache.fetch(
-      "#{studio_id}/unique_classes", expires_in: 2.hours
+      "studios/#{studio_id}/unique_classes", expires_in: 2.hours
     ) do
       Klass.where(studio_id: studio_id).pluck(:name).uniq
     end
 
-    render json: { studio: studio, classes: classes,
-                   unique_classes: unique_classes
-                 }
+    render json: {
+             studio: studio, classes: classes,
+             unique_classes: unique_classes
+           }
   end
 
   def create
@@ -25,20 +28,28 @@ class StudiosController < ApplicationController
     scraper_class = "Scraper::#{provider}"
 
     studio = Studio.find_by(schedule_url: params[:url].strip)
+    scraper = scraper_class.constantize.new(params[:url], studio)
 
     if !studio
-      result = scraper_class.constantize.get_classes(params[:url])
-      studio = result[:studio]
       new_studio = true
+      studio = scraper.parse_studio
     end
 
     if new_studio || studio.updated_at.nil? ||
        studio.updated_at < Time.now - 21600
-      scraper_class.constantize.get_classes(params[:url]) if result.nil?
+      scraper.parse_classes
       studio.update(updated_at: Time.now)
+      invalidate_studio_cache(studio.id)
     end
 
     render json: { studio: studio }
+  end
+
+  def invalidate_studio_cache(id)
+    keys = [
+      "studios/#{id}/classes", "studios/#{id}/unique_classes"
+    ]
+    keys.each { |key| Rails.cache.delete(key) }
   end
 
 end
