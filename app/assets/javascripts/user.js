56a41d9491323e777ab503d7a011cc48abb1b634
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2017-01-22 22:34:05 -0500

    Make User constructor function

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,21 @@
+function User(user_id) {
+  this.id = id;
+
+  var that = this;
+  var notify = new Notify();
+
+  this.update = function(data) {
+    $.ajax({
+      url: '/users/'+that.id,
+      type: 'PUT',
+      data: data,
+      success: function(response) {
+        notify.build(response.msg, 'success');
+      },
+      error: function(err) {
+        console.log(err);
+        notify.build(err, 'error');
+      }
+    });
+  };
+}
